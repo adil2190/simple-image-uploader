4d22f40f2f07688538b35efb3177e56478063180
adil2190 <madil219076@gmail.com>
2023-01-14 17:44:11 +0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import BeforeUpload from "./components/BeforeUpload";
+
+function App() {
+  return (
+    <div className="bg-[#fafafa] h-screen w-screen">
+      <BeforeUpload />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/BeforeUpload.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function BeforeUpload() {
+  return <div>Before Upload</div>;
+}
+
+export default BeforeUpload;
